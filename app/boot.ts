f8e7f7606044994a0a3aa2b82baba898b3db61e2
suparera <suparera@gmail.com>
2016-02-03 11:34:46 +0700

    Initial commit

---
+++ app/boot.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by suparera on 28/01/2016.
+ */
+
+import {bootstrap} from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+
+bootstrap(AppComponent);
+
---
+++ app/hero.service.ts
@@ -0,0 +1,4 @@
+/**
+ * Created by suparera on 02/02/2016.
+ */
+import {Injectable} from 'angular2/core';
